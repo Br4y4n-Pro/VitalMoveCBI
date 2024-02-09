c3ea1f9c5c9b741dabded994c753a2d0112cbf06
WilliamB0517 <willischurro16@gmail.com>
2024-02-09 17:46:45 -0500

    Avances inicio evaluador

--- lib/pages/Evaluador/evaluadorsesion.dart
+++ lib/pages/Evaluador/evaluadorsesion.dart
@@ -7,18 +7,28 @@ class EvaluadorLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/');
-              },
-              icon: Icon(Icons.arrow_back))
-        ],
-      ),
+      appBar: nav(context),
       body: Center(
         child: Column(
           children: [
+            Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Image.asset("img/Logo.png"),
+                ),
+                Text(
+                  "Codigo de evaluador",
+                  style: TextStyle(
+                      fontSize: 20, 
+                      color: Color.fromARGB(237, 3, 5, 94),
+                      fontWeight:FontWeight.w800
+                      ),
+                ),
+                inputLogin("Ingrese codigo de evaluador"),
+              ],
+            )),
             ElevatedButton(
                 style: ElevatedButton.styleFrom(
                     fixedSize: Size(size.width * .7, size.height * .08),
@@ -33,10 +43,42 @@ class EvaluadorLogin extends StatelessWidget {
                 onPressed: () {
                   Navigator.pushReplacementNamed(context, '/evaluadorHome');
                 },
-                child: Text('Iniciar Home Evaluador')),
+                child: Text('Ingresar')),
           ],
         ),
       ),
     );
   }
+}
+
+PreferredSizeWidget nav(context) {
+  final size = MediaQuery.of(context).size;
+  return PreferredSize(
+    preferredSize: Size.fromHeight(size.height * 0.3),
+    child: Container(
+      width: size.width * 1,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("img/fondoPrincipal.jpg"), fit: BoxFit.cover),
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(size.width * 0.2),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget inputLogin(String campo) {
+  return Container(
+      width: 320,
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(34, 174, 174, 174),
+          borderRadius: BorderRadius.circular(50)),
+      child: TextField(
+          cursorColor: Colors.orangeAccent,
+          decoration: InputDecoration(
+              hintText: campo,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  borderSide: BorderSide.none))));
 }
