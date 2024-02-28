049af8604cf780d318b0b2c792b75cc81b8937c1
L1S1M4C0 <itslisimaco@mail.com>
2024-02-28 17:19:00 -0500

    Lisimaco:Creacion del test caminata backend

--- lib/Api/AllApi.dart
+++ lib/Api/AllApi.dart
@@ -3,13 +3,9 @@ import 'package:dio/dio.dart';
 class AllApi {
   //ferasApp.php
 
-<<<<<<< HEAD
-  static String url = 'http://10.207.80.143:3050/';
 
-=======
   static String url = 'http://192.168.56.:3050/';
->>>>>>> c4a76ca05261d08ce456af358c09f5cfe98e73f0
-  // ignore: prefer_final_fields
+
   static Dio _dio = Dio();
 
   static void configuteDio() {
