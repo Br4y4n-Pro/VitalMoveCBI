d5fe7888b76a286b1ab70d9c11a4de91a99abb23
Brayan Esteban Velásquez Valdes <139822731+Ksisjd@users.noreply.github.com>
2024-03-04 12:41:25 -0500

    Jennifer Yara: actualizacion de estadisticas

--- lib/pages/graficos/peso.dart
+++ lib/pages/graficos/peso.dart
@@ -1,145 +1,175 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:unique_simple_bar_chart/data_models.dart';
+import 'package:unique_simple_bar_chart/simple_bar_chart.dart';
+import 'package:vitalmovecbi/widgets/colores.dart';
+import 'package:vitalmovecbi/widgets/customappbar.dart';
+import 'package:vitalmovecbi/widgets/textperfil.dart';
 
 class PagePeso extends StatefulWidget {
-  const PagePeso({super.key});
+  const PagePeso({Key? key}) : super(key: key);
 
   @override
   State<PagePeso> createState() => _PagePesoState();
 }
 
 class _PagePesoState extends State<PagePeso> {
+  List<double> pesoData = [
+    25.5,
+    20.1,
+    22.3,
+    25.5,
+    24.9,
+    24.4,
+    23.8,
+    23.5,
+    23.6,
+    23.6,
+    21.5,
+    25.3
+  ];
+  String mensajeEstado =
+      "Felicidades, ¡has alcanzado un estado óptimo de salud! Según las estadísticas del mes, tu índice de masa corporal (IMC) se encuentra dentro del rango saludable, lo que indica que estás manteniendo un peso adecuado para tu altura. Mantener un peso saludable es fundamental para prevenir enfermedades crónicas y promover una vida plena y activa. ¡Sigue así y continúa con tus hábitos saludables para mantener tu bienestar a largo plazo!";
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: const Text("Estadisticas del Peso")),
-        body: Center(
-          child: Container(
-            height: 150,
-            margin: EdgeInsets.symmetric(horizontal: size.width * .1),
-            child: LineChart(
-              LineChartData(
-                minX: 0,
-                maxX: 10,
-                minY: 0,
-                maxY: 10,
-                backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-                borderData: FlBorderData(
-                  show: false,
-                  border: Border.all(color: Colors.transparent),
-                ),
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: [
-                      const FlSpot(0, 4),
-                      const FlSpot(1, 6),
-                      const FlSpot(2, 8),
-                      const FlSpot(3, 6.2),
-                      const FlSpot(4, 6),
-                      const FlSpot(5, 8),
-                      const FlSpot(6, 9),
-                      const FlSpot(7, 7),
-                      const FlSpot(8, 6),
-                      const FlSpot(9, 7.8),
-                      const FlSpot(10, 8),
-                    ],
-                    isCurved: true,
-                    gradient: const LinearGradient(
-                      colors: [
-                        Colors.purple,
-                        Colors.pink,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colores.quaternaryColor),
+          title: const Text(
+            "Estadisticas del Peso",
+            style: TextStyle(
+                color: Colores.quaternaryColor, fontWeight: FontWeight.w500),
+          ),
+          backgroundColor: Colores.primaryColor,
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  Container(
+                    child: Column(
+                      children: [
+                        textSub(
+                            "Mi record de Enero a Diciembre"), // Función que devuelve un widget de texto con estilo
+                        // Función que devuelve un widget de texto con estilo
                       ],
                     ),
-                    barWidth: 3,
-                    belowBarData: BarAreaData(
-                      show: true,
-                      gradient: LinearGradient(
-                        colors: [
-                          Colors.purple.withOpacity(0.2),
-                          Colors.pink.withOpacity(0.2),
-                        ],
-                      ),
-                    ),
-                    dotData: const FlDotData(show: false),
                   ),
-                ],
-                gridData: FlGridData(
-                    show: true,
-                    drawHorizontalLine: false,
-                    drawVerticalLine: true,
-                    getDrawingVerticalLine: (value) {
-                      return const FlLine(
-                        color: Color.fromARGB(255, 163, 160, 160),
-                        strokeWidth: 0.8,
-                      );
-                    }),
-                titlesData: FlTitlesData(
-                  leftTitles: const AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  rightTitles: const AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  topTitles: const AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  bottomTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 12,
-                        getTitlesWidget: (value, meta) {
-                          String text = '';
-                          switch (value.toInt()) {
-                            case 1:
-                              text = "1";
-                              break;
-                            case 2:
-                              text = "2";
-                              break;
-                            case 3:
-                              text = "3";
-                              break;
-                            case 4:
-                              text = "4";
-                              break;
-                            case 5:
-                              text = "5";
-                              break;
-                            case 6:
-                              text = "6";
-                              break;
-                            case 7:
-                              text = "7";
-                              break;
-                            case 8:
-                              text = "8";
-                              break;
-                            case 9:
-                              text = "9";
-                              break;
-                            case 10:
-                              text = "10";
-                              break;
-                            default:
-                              return Container();
-                          }
-                          return Text(
-                            text,
-                            style: const TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              fontSize: 10,
+                  const SizedBox(height: 40),
+                  GestureDetector(
+                    onTap: () {
+                      // Aquí puedes mostrar una tarjeta de información (card) al seleccionar una barra
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text(
+                                    'Mi estado actual',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text(
+                                    "Felicidades, ¡has alcanzado un estado óptimo de salud! Según las estadísticas del mes, tu índice de masa corporal (IMC) se encuentra dentro del rango saludable, lo que indica que estás manteniendo un peso adecuado para tu altura. Mantener un peso saludable es fundamental para prevenir enfermedades crónicas y promover una vida plena y activa. ¡Sigue así y continúa con tus hábitos saludables para mantener tu bienestar a largo plazo!",
+                                  ),
+                                ),
+                              ],
                             ),
                           );
-                        }),
+                        },
+                      );
+                    },
+                    child: SimpleBarChart(
+                      listOfHorizontalBarData:
+                          pesoData.asMap().entries.map((entry) {
+                        final index = entry.key;
+                        final peso = entry.value;
+                        return HorizontalDetailsModel(
+                          name: _getMonthName(index +
+                              1), // Assuming index 0 corresponds to January
+                          color: Color.fromARGB(255, 6, 158, 223),
+                          size: peso, // Peso en kilogramos
+                        );
+                      }).toList(),
+                      verticalInterval:
+                          18, // Intervalo de 50 kg entre cada número
+                      horizontalBarPadding:
+                          3, // Reducción de la distancia entre las barras
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
-          ),
+            SizedBox(height: 30),
+            Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Mi estado actual',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      "Felicidades, ¡has alcanzado un estado óptimo de salud! Según las estadísticas del mes, tu índice de masa corporal (IMC) se encuentra dentro del rango saludable, lo que indica que estás manteniendo un peso adecuado para tu altura. Mantener un peso saludable es fundamental para prevenir enfermedades crónicas y promover una vida plena y activa. ¡Sigue así y continúa con tus hábitos saludables para mantener tu bienestar a largo plazo!",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
+        bottomNavigationBar: bottombar(context, 3),
       ),
     );
+  }
+
+  String _getMonthName(int month) {
+    switch (month) {
+      case 1:
+        return 'Ene';
+      case 2:
+        return 'Feb';
+      case 3:
+        return 'Mar';
+      case 4:
+        return 'Abr';
+      case 5:
+        return 'May';
+      case 6:
+        return 'Jun';
+      case 7:
+        return 'Jul';
+      case 8:
+        return 'Ago';
+      case 9:
+        return 'Sep';
+      case 10:
+        return 'Oct';
+      case 11:
+        return 'Nov';
+      case 12:
+        return 'Dic';
+      default:
+        return '';
+    }
   }
 }
